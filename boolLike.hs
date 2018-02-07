95f9b2669326cbbd34f0a919242c984d03260ed2
佐藤利徳 <tyahhahha@gmail.com>
2018-02-07 23:11:23 +0900

    up to 26-4

--- boolLike.hs
+++ boolLike.hs
@@ -27,3 +27,9 @@ instance BoolLike Float where
 instance BoolLike Double where
   toBool 0.0 = False
   toBool _ = True
+
+ifm :: BoolLike b => b -> a -> a -> a
+ifm b x y = if toBool b then x else y
+
+trues :: BoolLike b => [b] -> [b]
+trues = filter toBool
