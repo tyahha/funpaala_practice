8fff7a9a8288a2826d6f6ec654fa6e1af4391267
佐藤利徳 <tyahhahha@gmail.com>
2018-02-10 00:28:18 +0900

    up to 29-4

--- monoid.hs
+++ monoid.hs
@@ -1,3 +1,6 @@
+import Funpaala
+import Data.Monoid (Endo(..))
+
 newtype Maximum a = Maximum { getMaximum :: a } deriving Show
 
 instance (Ord a, Bounded a) => Monoid (Maximum a) where
@@ -9,3 +12,16 @@ newtype Minimum a = Minimum { getMinimum :: a } deriving Show
 instance (Ord a, Bounded a) => Monoid (Minimum a) where
   mempty = Minimum maxBound
   Minimum x `mappend` Minimum y = Minimum $ x `min` y
+
+ffldr :: (a -> b -> b) -> [a] -> b -> b
+ffldr = flip . fldr
+
+fldMap :: Monoid m => (a -> m) -> [a] -> m
+fldMap f (x : xs) = f x `mappend` fldMap f xs
+fldMap _ [] = mempty
+
+ffldr' :: (a -> b -> b) -> [a] -> b -> b
+ffldr' f = appEndo . fldMap (Endo . f)
+
+fldMap' :: Monoid m => (a -> m) -> [a] -> m
+fldMap' f = fldr (mappend . f) mempty
